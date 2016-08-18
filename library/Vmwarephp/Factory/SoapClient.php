8758cba3c76dea1e5d637e59601ec058d629687b
Damien Gardner Jnr <damien.gardner@serversaustralia.com.au>
2016-08-18 15:42:33 +1000

    fixing a couple of issues (incorrect autoload, and failing on self-signed SSL certs)

--- library/Vmwarephp/Factory/SoapClient.php
+++ library/Vmwarephp/Factory/SoapClient.php
@@ -18,7 +18,16 @@ class SoapClient {
 			'exceptions' => $useExceptions,
 			'connection_timeout' => 10,
 			'classmap' => $this->wsdlClassMapper->getClassMap(),
-			'features' => SOAP_SINGLE_ELEMENT_ARRAYS + SOAP_USE_XSI_ARRAY_TYPE
+      'features' => SOAP_SINGLE_ELEMENT_ARRAYS + SOAP_USE_XSI_ARRAY_TYPE,
+      'stream_context'=> stream_context_create(
+        array(
+          'ssl'=> array(
+           'verify_peer'=>false,
+          'verify_peer_name'=>false, 
+          'allow_self_signed' => true //can fiddle with this one.
+          )
+        )
+      )
 		);
 		$soapClient = $this->makeDefaultSoapClient($this->wsdlFilePath, $options);
 		if (!$soapClient) throw new Ex\CannotCreateSoapClient();
--- library/Vmwarephp/WsdlClassMapper.php
+++ library/Vmwarephp/WsdlClassMapper.php
@@ -65,7 +65,7 @@ class WsdlClassMapper {
 	private function cacheClassMap($classMap) {
 		if (!$this->useClassMapCaching) return;
 		if (!file_put_contents($this->getClassMapCacheFile(), serialize($classMap))) {
-			throw new \Exception('\\Vmwarephp\\WsdlClassMapper is configured to cache the class map but was not able to. Check the permissions on the cache directory.');
+			throw new \Exception('\\Vmwarephp\\WsdlClassMapper is configured to cache the class map ('.$this->getClassMapCacheFile().') but was not able to. Check the permissions on the cache directory.');
 		}
 	}
 
